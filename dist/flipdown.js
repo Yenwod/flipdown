341148f7868bd1b59a945b647a9fcd8f944026ec
Chris <cdowney@gmail.com>
2024-05-10 15:42:39 -0400

    Add default export so I can use in Rails 6 app with webpacker and Stimulus

--- dist/flipdown.js
+++ dist/flipdown.js
@@ -1,29 +1,27 @@
 "use strict";
 
-function _typeof(obj) { if (typeof Symbol === "function" && typeof Symbol.iterator === "symbol") { _typeof = function _typeof(obj) { return typeof obj; }; } else { _typeof = function _typeof(obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj; }; } return _typeof(obj); }
-
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports["default"] = void 0;
+function _typeof(o) { "@babel/helpers - typeof"; return _typeof = "function" == typeof Symbol && "symbol" == typeof Symbol.iterator ? function (o) { return typeof o; } : function (o) { return o && "function" == typeof Symbol && o.constructor === Symbol && o !== Symbol.prototype ? "symbol" : typeof o; }, _typeof(o); }
 function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
-
-function _defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } }
-
-function _createClass(Constructor, protoProps, staticProps) { if (protoProps) _defineProperties(Constructor.prototype, protoProps); if (staticProps) _defineProperties(Constructor, staticProps); return Constructor; }
-
+function _defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, _toPropertyKey(descriptor.key), descriptor); } }
+function _createClass(Constructor, protoProps, staticProps) { if (protoProps) _defineProperties(Constructor.prototype, protoProps); if (staticProps) _defineProperties(Constructor, staticProps); Object.defineProperty(Constructor, "prototype", { writable: false }); return Constructor; }
+function _toPropertyKey(t) { var i = _toPrimitive(t, "string"); return "symbol" == _typeof(i) ? i : i + ""; }
+function _toPrimitive(t, r) { if ("object" != _typeof(t) || !t) return t; var e = t[Symbol.toPrimitive]; if (void 0 !== e) { var i = e.call(t, r || "default"); if ("object" != _typeof(i)) return i; throw new TypeError("@@toPrimitive must return a primitive value."); } return ("string" === r ? String : Number)(t); }
 var FlipDown = function () {
   function FlipDown(uts) {
     var el = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : "flipdown";
     var opt = arguments.length > 2 && arguments[2] !== undefined ? arguments[2] : {};
-
     _classCallCheck(this, FlipDown);
-
     if (typeof uts !== "number") {
       throw new Error("FlipDown: Constructor expected unix timestamp, got ".concat(_typeof(uts), " instead."));
     }
-
     if (_typeof(el) === "object") {
       opt = el;
       el = "flipdown";
     }
-
     this.version = "0.3.2";
     this.initialised = false;
     this.now = this._getTime();
@@ -43,13 +41,10 @@ var FlipDown = function () {
     this.clockValuesAsString = [];
     this.prevClockValuesAsString = [];
     this.opts = this._parseOptions(opt);
-
     this._setOptions();
-
     console.log("FlipDown ".concat(this.version, " (Theme: ").concat(this.opts.theme, ")"));
   }
-
-  _createClass(FlipDown, [{
+  return _createClass(FlipDown, [{
     key: "start",
     value: function start() {
       if (!this.initialised) this._init();
@@ -63,7 +58,6 @@ var FlipDown = function () {
         cb();
         this.hasEndedCallback = null;
       };
-
       return this;
     }
   }, {
@@ -76,12 +70,10 @@ var FlipDown = function () {
     value: function _hasCountdownEnded() {
       if (this.epoch - this.now < 0) {
         this.countdownEnded = true;
-
         if (this.hasEndedCallback != null) {
           this.hasEndedCallback();
           this.hasEndedCallback = null;
         }
-
         return true;
       } else {
         this.countdownEnded = false;
@@ -92,11 +84,9 @@ var FlipDown = function () {
     key: "_parseOptions",
     value: function _parseOptions(opt) {
       var headings = ["Days", "Hours", "Minutes", "Seconds"];
-
       if (opt.headings && opt.headings.length === 4) {
         headings = opt.headings;
       }
-
       return {
         theme: opt.hasOwnProperty("theme") ? opt.theme : "dark",
         headings: headings
@@ -111,48 +101,35 @@ var FlipDown = function () {
     key: "_init",
     value: function _init() {
       this.initialised = true;
-
       if (this._hasCountdownEnded()) {
         this.daysremaining = 0;
       } else {
         this.daysremaining = Math.floor((this.epoch - this.now) / 86400).toString().length;
       }
-
       var dayRotorCount = this.daysremaining <= 2 ? 2 : this.daysremaining;
-
       for (var i = 0; i < dayRotorCount + 6; i++) {
         this.rotors.push(this._createRotor(0));
       }
-
       var dayRotors = [];
-
       for (var i = 0; i < dayRotorCount; i++) {
         dayRotors.push(this.rotors[i]);
       }
-
       this.element.appendChild(this._createRotorGroup(dayRotors, 0));
       var count = dayRotorCount;
-
       for (var i = 0; i < 3; i++) {
         var otherRotors = [];
-
         for (var j = 0; j < 2; j++) {
           otherRotors.push(this.rotors[count]);
           count++;
         }
-
         this.element.appendChild(this._createRotorGroup(otherRotors, i + 1));
       }
-
       this.rotorLeafFront = Array.prototype.slice.call(this.element.getElementsByClassName("rotor-leaf-front"));
       this.rotorLeafRear = Array.prototype.slice.call(this.element.getElementsByClassName("rotor-leaf-rear"));
       this.rotorTop = Array.prototype.slice.call(this.element.getElementsByClassName("rotor-top"));
       this.rotorBottom = Array.prototype.slice.call(this.element.getElementsByClassName("rotor-bottom"));
-
       this._tick();
-
       this._updateClockValues(true);
-
       return this;
     }
   }, {
@@ -202,16 +179,13 @@ var FlipDown = function () {
       this.clockValues.m = Math.floor(diff / 60);
       diff -= this.clockValues.m * 60;
       this.clockValues.s = Math.floor(diff);
-
       this._updateClockValues();
-
       this._hasCountdownEnded();
     }
   }, {
     key: "_updateClockValues",
     value: function _updateClockValues() {
       var _this = this;
-
       var init = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : false;
       this.clockStrings.d = pad(this.clockValues.d, 2);
       this.clockStrings.h = pad(this.clockValues.h, 2);
@@ -224,20 +198,16 @@ var FlipDown = function () {
       this.rotorBottom.forEach(function (el, i) {
         el.textContent = _this.prevClockValuesAsString[i];
       });
-
       function rotorTopFlip() {
         var _this2 = this;
-
         this.rotorTop.forEach(function (el, i) {
           if (el.textContent != _this2.clockValuesAsString[i]) {
             el.textContent = _this2.clockValuesAsString[i];
           }
         });
       }
-
       function rotorLeafRearFlip() {
         var _this3 = this;
-
         this.rotorLeafRear.forEach(function (el, i) {
           if (el.textContent != _this3.clockValuesAsString[i]) {
             el.textContent = _this3.clockValuesAsString[i];
@@ -249,7 +219,6 @@ var FlipDown = function () {
           }
         });
       }
-
       if (!init) {
         setTimeout(rotorTopFlip.bind(this), 500);
         setTimeout(rotorLeafRearFlip.bind(this), 500);
@@ -257,21 +226,17 @@ var FlipDown = function () {
         rotorTopFlip.call(this);
         rotorLeafRearFlip.call(this);
       }
-
       this.prevClockValuesAsString = this.clockValuesAsString;
     }
   }]);
-
-  return FlipDown;
 }();
-
 function pad(n, len) {
   n = n.toString();
   return n.length < len ? pad("0" + n, len) : n;
 }
-
 function appendChildren(parent, children) {
   children.forEach(function (el) {
     parent.appendChild(el);
   });
 }
+var _default = exports["default"] = FlipDown;
